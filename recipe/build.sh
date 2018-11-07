7a04521b30f6377c825ea1bd1ce547175ecc7803
looooo <sppedflyer@gmail.com>
2018-11-07 14:16:51 +0100

    commit for triggering builds again

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,8 +4,10 @@ if [[ ${HOST} =~ .*darwin.* ]]; then
   export CPPFLAGS="${CPPFLAGS} -framework OpenGL"
   export LDFLAGS="${LDFLAGS} -framework OpenGL"
 fi
+
 ./configure --prefix=${PREFIX} \
             --build=${BUILD} \
             --host=${HOST}
+
 make -j${CPU_COUNT} ${VERBOSE_AT}
 make install
